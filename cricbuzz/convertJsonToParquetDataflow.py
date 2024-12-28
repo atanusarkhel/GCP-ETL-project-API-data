5d0f6c0e9fd9aa40175fbdf195e65714b8705d16
Atanu Sarkhel <atanusarkhel96@gmail.com>
2024-12-28 14:31:24 +0530

    beam code added to convert json to parquet

--- cricbuzz/convertJsonToParquetDataflow.py
+++ cricbuzz/convertJsonToParquetDataflow.py
@@ -7,14 +7,8 @@ import pyarrow.parquet as pq
 import argparse
 import logging
 
-def jsonToDict(json_data):
-    try:
-        schema=pa.schema([(key, pa.string()) for key in json_data.keys()])
-        return schema
-    except Exception as e:
-        print(f"Error - {e}")
-        return None
 
+# convert json data to dictionary
 def jsonToDict(json_string):
     try:
         dict_data=json.loads(json_string)
@@ -23,6 +17,18 @@ def jsonToDict(json_string):
         print(f"Error - {e}")
         return None
 
+
+# get pyarrow table schema dynamically from json data(dictionary)
+def inferSchemaFromJson(json_data):
+    try:
+        schema=pa.schema([(key, pa.string()) for key in json_data.keys()])
+        return schema
+    except Exception as e:
+        print(f"Error - {e}")
+        return None
+
+
+# convert json_data(dictionary) to pyarrow table for writing into parquet
 def convertToArrowTable(json_data,schema):
     try:
         pa_table_Data=pa.table([json_data],schema=schema)
@@ -33,18 +39,46 @@ def convertToArrowTable(json_data,schema):
         return None
 
 
+# writing json data into parquet
+def writeToParquet(json_data,schema,output_file):
+    try:
+        pa_table=pa.table(json_data,schema=schema)
+        pq.write_table(pa_table, output_file, compression='snappy')
+    except Exception as e:
+        print(f"Error writing to parquet- {e}")
+        return None
 
 options= PipelineOptions(
     flags=None,
     runner='DataflowRunner',
     project="famous-crossing-445714-p4",
     region="us-east1",
-    temp_location="gs://cricbuzz/temp",
-    staging_location="gs://cricbuzz/staging_loc",
-    output_location="gs://cricbuzz/transform_parquet",
+    temp_location="gs://cricbuzz/temp/",
+    staging_location="gs://cricbuzz/staging_loc/",
+    output_location="gs://cricbuzz/transform_parquet/",
     job_name="json_to_parquet"
 )
 
-def run():
+def run(input_folder,output_folder):
     with beam.Pipeline(options=options) as p:
-        pass
+        input_files=(
+            p
+            | 'read Files' >> beam.io.ReadFromText(input_folder)
+            | 'parse json' >> beam.Map(jsonToDict)
+            | 'Infer Schema' >> beam.Map(lambda x: inferSchemaFromJson(x) if x else None)
+        )
+
+        schema=(
+            input_files
+            | 'extract schema' >> beam.Filter(lambda x : x if x else None)
+            | 'First Schema'  >> beam.Top.Of(1, key=lambda x: 1)
+            | 'Get Schema'   >> beam.Map(lambda x: x[0])
+        )
+        input_files | 'Convert to Arrow table' >> beam.Map(lambda x: convertToArrowTable(x,schema) if x else None) \
+                    | 'Write to Parquet' >> beam.Map(lambda x : writeToParquet([x],schema, output_folder))
+
+if __name__=='__main__':
+    input_folder='gs://cricbuzz/raw_json/'
+    output_folder='gs://cricbuzz/output-folder/'
+
+    run(input_folder,output_folder)
